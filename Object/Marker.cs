6980550b35cfe64ded043bcff6210d9dfca07fd6
egorn <egor.nazarenko31@mail.ru>
2023-01-12 10:13:37 +0800

    Добавил все нужные классы

---
+++ Object/Marker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labe5.Object
+{
+    internal class Marker
+    {
+    }
+}
