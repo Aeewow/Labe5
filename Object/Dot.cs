223cb504ede183375c7d19ebe88c085b1b9ef9aa
egorn <egor.nazarenko31@mail.ru>
2023-01-12 10:16:51 +0800

    Добавил внутренности рандомных точек

--- Object/Dot.cs
+++ Object/Dot.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Labe5.Object
 {
-    internal class Dot
+    internal class Dot : BaseObject
     {
+        public float radius = 50;
+        public Action<Dot> onDeath;
+
+        public Dot(float x, float y, float angle, float radius, int timer) : base(x, y, angle)
+        {
+
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.GreenYellow), -radius / 2, -radius / 2, radius, radius);
+            this.radius -= (float)0.5;
+            if (radius <= 0)
+            {
+                radius = 50;
+                onDeath(this);
+            }
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-radius / 2, -radius / 2, radius, radius);
+            return path;
+        }
     }
 }
