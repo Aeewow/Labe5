bf470db6cc9efe1523895aec6c7e53561af99c0e
egorn <egor.nazarenko31@mail.ru>
2023-01-12 10:08:37 +0800

    Начало работы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.Score_Label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(862, 28);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(303, 616);
+            this.txtLog.TabIndex = 2;
+            this.txtLog.Text = "";
+            // 
+            // pbMain
+            // 
+            this.pbMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbMain.Location = new System.Drawing.Point(13, 4);
+            this.pbMain.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(829, 666);
+            this.pbMain.TabIndex = 3;
+            this.pbMain.TabStop = false;
+            // 
+            // Score_Label
+            // 
+            this.Score_Label.AutoSize = true;
+            this.Score_Label.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Score_Label.Location = new System.Drawing.Point(33, 28);
+            this.Score_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Score_Label.Name = "Score_Label";
+            this.Score_Label.Size = new System.Drawing.Size(79, 28);
+            this.Score_Label.TabIndex = 4;
+            this.Score_Label.Text = "Счет: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1179, 676);
+            this.Controls.Add(this.Score_Label);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private RichTextBox txtLog;
+        private PictureBox pbMain;
+        private Label Score_Label;
+        private System.Windows.Forms.Timer timer1;
     }
 }
